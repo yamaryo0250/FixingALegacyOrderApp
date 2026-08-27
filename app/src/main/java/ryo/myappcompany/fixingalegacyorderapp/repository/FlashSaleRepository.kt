9ce28429ae5faf9e479f64f199c31efa0212cac5
Ryo Yamagiwa <ryo.yamagiwa0250@gmail.com>
2026-08-27 21:54:08 +0900

    repository層を実装

--- app/src/main/java/ryo/myappcompany/fixingalegacyorderapp/repository/FlashSaleRepository.kt
+++ app/src/main/java/ryo/myappcompany/fixingalegacyorderapp/repository/FlashSaleRepository.kt
@@ -1,4 +1,25 @@
 package ryo.myappcompany.fixingalegacyorderapp.repository
 
+import ryo.myappcompany.fixingalegacyorderapp.domain.ProductInfo
+import ryo.myappcompany.fixingalegacyorderapp.domain.PurchaseResult
+
+/**
+ * フラッシュセールリポジトリ interface
+ */
 interface FlashSaleRepository {
+
+    /**
+     * 商品詳細情報取得
+     *
+     * @return 商品詳細情報(ドメインクラス)
+     */
+    suspend fun loadProductDetails(): ProductInfo
+
+    /**
+     * 商品購入処理
+     *
+     * @param itemId 商品ID
+     * @return 購入処理結果
+     */
+    suspend fun purchaseItem(itemId: String): PurchaseResult
 }
--- app/src/main/java/ryo/myappcompany/fixingalegacyorderapp/repository/FlashSaleRepositoryImpl.kt
+++ app/src/main/java/ryo/myappcompany/fixingalegacyorderapp/repository/FlashSaleRepositoryImpl.kt
@@ -1,4 +1,50 @@
 package ryo.myappcompany.fixingalegacyorderapp.repository
 
-class FlashSaleRepositoryImpl {
+import ryo.myappcompany.fixingalegacyorderapp.domain.ProductInfo
+import ryo.myappcompany.fixingalegacyorderapp.domain.PurchaseResult
+import ryo.myappcompany.fixingalegacyorderapp.manager.OrderManager
+
+/**
+ * フラッシュセールリポジトリ 実装
+ */
+class FlashSaleRepositoryImpl(
+    private val orderManager: OrderManager
+) : FlashSaleRepository {
+
+    /**
+     * 商品詳細情報取得
+     *
+     * @return 商品詳細情報(ドメインクラス)
+     */
+    override suspend fun loadProductDetails(): ProductInfo {
+        return try {
+            orderManager.loadProductDetails()
+        } catch (e: Exception) {
+            if (e is kotlinx.coroutines.CancellationException) {
+                throw e
+            }
+
+            throw ApiConnectException(e)
+        }
+    }
+
+    /**
+     * 商品購入処理
+     *
+     * @param itemId 商品ID
+     * @return 購入処理結果
+     */
+    override suspend fun purchaseItem(itemId: String): PurchaseResult {
+        return try {
+            orderManager.purchaseItem(itemId)
+        } catch (e: Exception) {
+            if (e is kotlinx.coroutines.CancellationException) {
+                throw e
+            }
+
+            throw ApiConnectException(e)
+        }
+    }
 }
+
+class ApiConnectException(cause: Throwable? = null) : Exception(cause)
