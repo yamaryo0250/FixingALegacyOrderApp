f7f8c12ce2d12d8573034b19a7071ba52935af84
Ryo Yamagiwa <ryo.yamagiwa0250@gmail.com>
2026-08-27 11:01:00 +0900

    KDocを追記

--- app/src/main/java/ryo/myappcompany/fixingalegacyorderapp/domain/PurchaseResult.kt
+++ app/src/main/java/ryo/myappcompany/fixingalegacyorderapp/domain/PurchaseResult.kt
@@ -1,6 +1,18 @@
 package ryo.myappcompany.fixingalegacyorderapp.domain
 
+/**
+ * 購入処理結果
+ */
 sealed class PurchaseResult
 
+/**
+ * 購入処理成功
+ */
 object Success : PurchaseResult()
+
+/**
+ * 購入処理失敗
+ *
+ * @param cause 処理の失敗内容
+ */
 class Failure(cause: String) : PurchaseResult()
